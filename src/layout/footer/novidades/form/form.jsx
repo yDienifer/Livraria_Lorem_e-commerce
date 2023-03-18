e09f074d2c6a87e778fc6656ba15dae083029f18
yDienifer <dienifersiqueira.dev@gmail.com>
2023-03-18 14:54:27 -0300

    Neste commit, foram realizadas algumas melhorias na responsividade, corrigido um problema no card do e-book e tornado o formulário funcional.

--- src/layout/footer/novidades/form/form.jsx
+++ src/layout/footer/novidades/form/form.jsx
@@ -1,14 +1,37 @@
 import styles from "./form.module.scss";
 
 export const FormNovidades = () => {
+  window.onload = function () {
+    
+    let botao = document.getElementById("botao-enviar");
+
+    botao.onclick = function (e) {
+      var inputEmail = document.getElementById("input-email");
+      const emailDoUsuário = inputEmail.value;
+
+      if (inputEmail.value == "") {
+        inputEmail.style.border = "1px solid Red";
+        botao.style.backgroundColor = "Red";
+      } else {
+        if (confirm("Confirmou se todos os dados estão corretos?")) {
+          botao.style.backgroundColor = "Green";
+          alert(`Parabéns! O email ${emailDoUsuário} foi enviado com sucesso!`);
+        } else {
+          e.preventDefault(); // Evita que o form seja enviado automaticamente
+        }
+      }
+    };
+  };
+
   return (
-    <form
-      action="/pagina-processa-dados-do-form"
-      method="post"
-      className={styles.formContainer}
-    >
-      <input type="email" placeholder="Adicione o seu e-mail...." required />
-      <input type="submit" value="Enviar" />
+    <form className={styles.formContainer}>
+      <input
+        type="email"
+        placeholder="Adicione o seu e-mail...."
+        required
+        id="input-email"
+      />
+      <input type="submit" value="Enviar" id="botao-enviar" />
     </form>
   );
 };
