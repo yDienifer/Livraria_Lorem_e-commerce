58bbd779d3086419761f596e31928ed57aca7fb2
yDienifer <dienifersiqueira.dev@gmail.com>
2023-03-19 17:55:43 -0300

    Neste commit, eu melhorei um pouco mais a qualidade da responsividade do card de e-book e do atalho 'Sobre' no rodapé.

--- src/components/card-e-book/card-e-book.jsx
+++ src/components/card-e-book/card-e-book.jsx
@@ -16,12 +16,16 @@ export const CardEbook = ({
   useEffect(() => {
     AOS.init({ duration: 1950 });
   }, []);
-  
+
   return (
     <div className={styles.cardEbookContainer} data-aos="fade-up">
       <div className={styles.card}>
         <img src={imgCard} alt={altImgCard} className={styles.imgCard} />
-        <div className={`${styles.infoEbook} ${precoOriginalDoEbook ? "" : styles.semPromocao}`}>
+        <div
+          className={`${styles.infoEbook} ${
+            precoOriginalDoEbook ? "" : styles.semPromocao
+          }`}
+        >
           <h1 className={styles.nomeDoEbook}>{nomeDoEbook}</h1>
           <span className={styles.categoriaDoEbook}>{categoriaDoEbook}</span>
           <span className={styles.descricaoDoEbook}>{descricaoDoEbook}</span>
