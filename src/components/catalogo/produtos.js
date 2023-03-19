098a8ba095ac07683faa053db676d49c084c29a2
yDienifer <dienifersiqueira.dev@gmail.com>
2023-03-19 15:34:26 -0300

    Neste commit, realizei alterações nos e-books da seção Promoção. Adicionei livros educacionais à seção e organizei os e-books em subpastas dentro da pasta 'img', renomeando-a de 'Products' para 'E-books'. Além disso, atualizei os preços de alguns dos e-books.

--- src/components/catalogo/produtos.js
+++ src/components/catalogo/produtos.js
@@ -1,7 +1,7 @@
 export const LivrosTerror = [
   {
     id: 1,
-    imgCard: "static/img/products/Deserunt_tempor.png",
+    imgCard: "static/img/e-books/horror/Deserunt_tempor.png",
     altImgCard: "Deserunt tempor",
     nomeDoProduto: "Deserunt tempor",
     descricaoDoProduto:
@@ -10,7 +10,7 @@ export const LivrosTerror = [
   },
   {
     id: 2,
-    imgCard: "static/img/products/Laboris_ipsum.png",
+    imgCard: "static/img/e-books/horror/Laboris_ipsum.png",
     altImgCard: "Laboris ipsum",
     nomeDoProduto: "Laboris ipsum",
     descricaoDoProduto:
@@ -19,16 +19,16 @@ export const LivrosTerror = [
   },
   {
     id: 3,
-    imgCard: "static/img/products/Lorem_ipsum.png",
-    altImgCard: "Lorem ipsum",
-    nomeDoProduto: "Lorem ipsum",
+    imgCard: "static/img/e-books/horror/Lorem_dolor.png",
+    altImgCard: "Lorem dolor",
+    nomeDoProduto: "Lorem dolor",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     precoPromocaoDoProduto: "R$ 46,42",
   },
   {
     id: 4,
-    imgCard: "static/img/products/Occafcat_fugit.png",
+    imgCard: "static/img/e-books/horror/Occafcat_fugit.png",
     altImgCard: "Occafcat fugit",
     nomeDoProduto: "Occafcat fugit",
     descricaoDoProduto:
@@ -37,7 +37,7 @@ export const LivrosTerror = [
   },
   {
     id: 5,
-    imgCard: "static/img/products/Magna_aliqua.png",
+    imgCard: "static/img/e-books/horror/Magna_aliqua.png",
     altImgCard: "Magna aliqua",
     nomeDoProduto: "Magna aliqua",
     descricaoDoProduto:
@@ -46,7 +46,7 @@ export const LivrosTerror = [
   },
   {
     id: 6,
-    imgCard: "static/img/products/The_dolor_veniam.png",
+    imgCard: "static/img/e-books/horror/The_dolor_veniam.png",
     altImgCard: "The dolor veniam",
     nomeDoProduto: "The dolor veniam",
     descricaoDoProduto:
@@ -56,7 +56,7 @@ export const LivrosTerror = [
   },
   {
     id: 7,
-    imgCard: "static/img/products/Eiusmod_lorem.png",
+    imgCard: "static/img/e-books/horror/Eiusmod_lorem.png",
     altImgCard: "Eiusmod lorem",
     nomeDoProduto: "Eiusmod lorem",
     descricaoDoProduto:
@@ -65,7 +65,7 @@ export const LivrosTerror = [
   },
   {
     id: 8,
-    imgCard: "static/img/products/Auto_irure_lorem.png",
+    imgCard: "static/img/e-books/horror/Auto_irure_lorem.png",
     altImgCard: "Auto irure lorem",
     nomeDoProduto: "Auto irure lorem",
     descricaoDoProduto:
@@ -78,7 +78,7 @@ export const LivrosTerror = [
 export const LivrosRomance = [
   {
     id: 1,
-    imgCard: "static/img/products/Laboris_nisi.png",
+    imgCard: "static/img/e-books/romance/Laboris_nisi.png",
     altImgCard: "Laboris nisi",
     nomeDoProduto: "Laboris nisi",
     descricaoDoProduto:
@@ -88,7 +88,7 @@ export const LivrosRomance = [
   },
   {
     id: 2,
-    imgCard: "static/img/products/Animid_id_laborum.png",
+    imgCard: "static/img/e-books/romance/Animid_id_laborum.png",
     altImgCard: "Animid id laborum",
     nomeDoProduto: "Animid id laborum",
     descricaoDoProduto:
@@ -98,7 +98,7 @@ export const LivrosRomance = [
   },
   {
     id: 3,
-    imgCard: "static/img/products/Amet_elit.png",
+    imgCard: "static/img/e-books/romance/Amet_elit.png",
     altImgCard: "Amet elit!",
     nomeDoProduto: "Amet elit!",
     descricaoDoProduto:
@@ -107,7 +107,7 @@ export const LivrosRomance = [
   },
   {
     id: 4,
-    imgCard: "static/img/products/Sit_amet_lorem.png",
+    imgCard: "static/img/e-books/romance/Sit_amet_lorem.png",
     altImgCard: "Sit amet lorem",
     nomeDoProduto: "Sit amet lorem",
     descricaoDoProduto:
@@ -115,8 +115,8 @@ export const LivrosRomance = [
     precoPromocaoDoProduto: "R$ 45,90",
   },
   {
-    id: 4,
-    imgCard: "static/img/products/Lorem_ipsum2.png",
+    id: 5,
+    imgCard: "static/img/e-books/romance/Lorem_ipsum.png",
     altImgCard: "Lorem ipsum",
     nomeDoProduto: "Lorem ipsum",
     descricaoDoProduto:
@@ -124,8 +124,8 @@ export const LivrosRomance = [
     precoPromocaoDoProduto: "R$ 46,42",
   },
   {
-    id: 5,
-    imgCard: "static/img/products/Commodo_consequat.png",
+    id: 6,
+    imgCard: "static/img/e-books/romance/Commodo_consequat.png",
     altImgCard: "Commodo consequat",
     nomeDoProduto: "Commodo consequat",
     descricaoDoProduto:
@@ -133,8 +133,8 @@ export const LivrosRomance = [
     precoPromocaoDoProduto: "R$ 35,00",
   },
   {
-    id: 6,
-    imgCard: "static/img/products/Deserunt_officia.png",
+    id: 7,
+    imgCard: "static/img/e-books/romance/Deserunt_officia.png",
     altImgCard: "Deserunt officia",
     nomeDoProduto: "Deserunt officia",
     descricaoDoProduto:
@@ -142,8 +142,8 @@ export const LivrosRomance = [
     precoPromocaoDoProduto: "R$ 44,50",
   },
   {
-    id: 7,
-    imgCard: "static/img/products/Excepteur_sint.png",
+    id: 8,
+    imgCard: "static/img/e-books/romance/Excepteur_sint.png",
     altImgCard: "Excepteur sint",
     nomeDoProduto: "Excepteur sint",
     descricaoDoProduto:
@@ -155,7 +155,7 @@ export const LivrosRomance = [
 export const LivrosSuspense = [
   {
     id: 1,
-    imgCard: "static/img/products/Mollit_elit.png",
+    imgCard: "static/img/e-books/suspense/Mollit_elit.png",
     altImgCard: "Mollit elit",
     nomeDoProduto: "Mollit elit",
     descricaoDoProduto:
@@ -165,7 +165,7 @@ export const LivrosSuspense = [
   },
   {
     id: 2,
-    imgCard: "static/img/products/Incididunt_tempor.png",
+    imgCard: "static/img/e-books/suspense/Incididunt_tempor.png",
     altImgCard: "Incididunt tempor",
     nomeDoProduto: "Incididunt tempor",
     descricaoDoProduto:
@@ -174,7 +174,7 @@ export const LivrosSuspense = [
   },
   {
     id: 3,
-    imgCard: "static/img/products/Ipsum_sunt.png",
+    imgCard: "static/img/e-books/suspense/Ipsum_sunt.png",
     altImgCard: "Ipsum sunt",
     nomeDoProduto: "Ipsum sunt",
     descricaoDoProduto:
@@ -183,7 +183,7 @@ export const LivrosSuspense = [
   },
   {
     id: 4,
-    imgCard: "static/img/products/Minim_ipsum.png",
+    imgCard: "static/img/e-books/suspense/Minim_ipsum.png",
     altImgCard: "Minim ipsum",
     nomeDoProduto: "Minim ipsum",
     descricaoDoProduto:
@@ -192,7 +192,7 @@ export const LivrosSuspense = [
   },
   {
     id: 5,
-    imgCard: "static/img/products/Fugiat.png",
+    imgCard: "static/img/e-books/suspense/Fugiat.png",
     altImgCard: "Fugiat",
     nomeDoProduto: "Fugiat",
     descricaoDoProduto:
@@ -201,17 +201,16 @@ export const LivrosSuspense = [
   },
   {
     id: 6,
-    imgCard: "static/img/products/Tempor.png",
+    imgCard: "static/img/e-books/suspense/Tempor.png",
     altImgCard: "Tempor",
     nomeDoProduto: "Tempor",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     precoPromocaoDoProduto: "R$ 69,90",
   },
-
   {
     id: 7,
-    imgCard: "static/img/products/The_amet_lorem.png",
+    imgCard: "static/img/e-books/suspense/The_amet_lorem.png",
     altImgCard: "The amet lorem",
     nomeDoProduto: "The amet lorem",
     descricaoDoProduto:
@@ -220,7 +219,7 @@ export const LivrosSuspense = [
   },
   {
     id: 8,
-    imgCard: "static/img/products/Dolor_ut.png",
+    imgCard: "static/img/e-books/suspense/Dolor_ut.png",
     altImgCard: "Dolor ut",
     nomeDoProduto: "Dolor ut",
     descricaoDoProduto:
@@ -232,7 +231,7 @@ export const LivrosSuspense = [
 export const LivrosMaisVendidos = [
   {
     id: 1,
-    imgCard: "static/img/products/Occafcat_fugit.png",
+    imgCard: "static/img/e-books/horror/Occafcat_fugit.png",
     altImgCard: "Occafcat fugit",
     nomeDoProduto: "Occafcat fugit",
     descricaoDoProduto:
@@ -241,7 +240,7 @@ export const LivrosMaisVendidos = [
   },
   {
     id: 2,
-    imgCard: "static/img/products/Magna_aliqua.png",
+    imgCard: "static/img/e-books/horror/Magna_aliqua.png",
     altImgCard: "Magna aliqua",
     nomeDoProduto: "Magna aliqua",
     descricaoDoProduto:
@@ -250,7 +249,7 @@ export const LivrosMaisVendidos = [
   },
   {
     id: 3,
-    imgCard: "static/img/products/Amet_elit.png",
+    imgCard: "static/img/e-books/romance/Amet_elit.png",
     altImgCard: "Amet elit",
     nomeDoProduto: "Amet elit",
     descricaoDoProduto:
@@ -259,25 +258,27 @@ export const LivrosMaisVendidos = [
   },
   {
     id: 4,
-    imgCard: "static/img/products/Minim_ipsum.png",
-    altImgCard: "Minim ipsum",
-    nomeDoProduto: "Minim ipsum",
+    imgCard: "static/img/e-books/promotions/Lorem_elit.png",
+    altImgCard: "Lorem elit",
+    nomeDoProduto: "Lorem elit",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    precoPromocaoDoProduto: "R$ 35,99",
+    precoPromocaoDoProduto: "R$ 17,99",
+    precoOriginalDoProduto: "R$ 19,99",
   },
   {
     id: 5,
-    imgCard: "static/img/products/Lorem_ipsum2.png",
-    altImgCard: "Lorem ipsum",
-    nomeDoProduto: "Lorem ipsum",
+    imgCard: "static/img/e-books/promotions/Culpa_non.png",
+    altImgCard: "Culpa non",
+    nomeDoProduto: "Culpa non",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    precoPromocaoDoProduto: "R$ 46,42",
+    precoPromocaoDoProduto: "R$ 31,50",
+    precoOriginalDoProduto: "R$ 35,00",
   },
   {
     id: 6,
-    imgCard: "static/img/products/Fugiat.png",
+    imgCard: "static/img/e-books/suspense/Fugiat.png",
     altImgCard: "Fugiat",
     nomeDoProduto: "Fugiat",
     descricaoDoProduto:
@@ -287,7 +288,7 @@ export const LivrosMaisVendidos = [
 
   {
     id: 7,
-    imgCard: "static/img/products/Tempor.png",
+    imgCard: "static/img/e-books/suspense/Tempor.png",
     altImgCard: "Tempor",
     nomeDoProduto: "Tempor",
     descricaoDoProduto:
@@ -296,9 +297,9 @@ export const LivrosMaisVendidos = [
   },
   {
     id: 8,
-    imgCard: "static/img/products/Animid_id_laborum.png",
-    altImgCard: "Animad id laborum",
-    nomeDoProduto: "Animad id laborum",
+    imgCard: "static/img/e-books/promotions/Elit_ipsum.png",
+    altImgCard: "Elit ipsum",
+    nomeDoProduto: "Elit ipsum",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     precoPromocaoDoProduto: "R$ 36,00",
@@ -309,7 +310,7 @@ export const LivrosMaisVendidos = [
 export const Lançamentos = [
   {
     id: 1,
-    imgCard: "static/img/products/The_amet_lorem.png",
+    imgCard: "static/img/e-books/suspense/The_amet_lorem.png",
     altImgCard: "The amet lorem",
     nomeDoProduto: "The amet lorem",
     descricaoDoProduto:
@@ -318,9 +319,9 @@ export const Lançamentos = [
   },
   {
     id: 2,
-    imgCard: "static/img/products/Lorem_ipsum.png",
-    altImgCard: "Lorem ipsum",
-    nomeDoProduto: "Lorem ipsum",
+    imgCard: "static/img/e-books/horror/Lorem_dolor.png",
+    altImgCard: "Lorem dolor",
+    nomeDoProduto: "Lorem dolor",
     descricaoDoProduto:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     precoPromocaoDoProduto: "R$ 54,00",
@@ -328,7 +329,7 @@ export const Lançamentos = [
   },
   {
     id: 3,
-    imgCard: "static/img/products/Eiusmod_lorem.png",
+    imgCard: "static/img/e-books/horror/Eiusmod_lorem.png",
     altImgCard: "Euismod lorem",
     nomeDoProduto: "Euismod lorem",
     descricaoDoProduto:
@@ -337,7 +338,7 @@ export const Lançamentos = [
   },
   {
     id: 4,
-    imgCard: "static/img/products/Commodo_consequat.png",
+    imgCard: "static/img/e-books/romance/Commodo_consequat.png",
     altImgCard: "Commodo consequat",
     nomeDoProduto: "Commodo consequat",
     descricaoDoProduto:
@@ -346,7 +347,7 @@ export const Lançamentos = [
   },
   {
     id: 5,
-    imgCard: "static/img/products/Dolor_ut.png",
+    imgCard: "static/img/e-books/suspense/Dolor_ut.png",
     altImgCard: "Dolor ut",
     nomeDoProduto: "Dolor ut",
     descricaoDoProduto:
@@ -355,7 +356,7 @@ export const Lançamentos = [
   },
   {
     id: 6,
-    imgCard: "static/img/products/Sit_amet_lorem.png",
+    imgCard: "static/img/e-books/romance/Sit_amet_lorem.png",
     altImgCard: "Sit amet lorem",
     nomeDoProduto: "Sit amet lorem",
     descricaoDoProduto:
@@ -365,7 +366,7 @@ export const Lançamentos = [
 
   {
     id: 7,
-    imgCard: "static/img/products/Laboris_ipsum.png",
+    imgCard: "static/img/e-books/horror/Laboris_ipsum.png",
     altImgCard: "Laboris ipsum",
     nomeDoProduto: "Laboris ipsum",
     descricaoDoProduto:
@@ -374,7 +375,7 @@ export const Lançamentos = [
   },
   {
     id: 8,
-    imgCard: "static/img/products/Incididunt_tempor.png",
+    imgCard: "static/img/e-books/suspense/Incididunt_tempor.png",
     altImgCard: "Incididunt tempor",
     nomeDoProduto: "Incididunt tempor",
     descricaoDoProduto:
